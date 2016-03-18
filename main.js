549b69d11155007718f7db813a4afe897db4adcf
Noah Flick <noahflick@gmail.com>
2016-03-18 15:47:19 -0700

    First commit

---
+++ main.js
@@ -0,0 +1,38 @@
+console.log("linked")
+
+var hero = {
+  name:
+  actions:
+  equipped: [pen, computer, backpack]
+}
+var status
+var inventory
+var party
+var objective
+var equipped
+
+var screen
+var journalScreen
+var log
+
+var locations
+var
+
+function drawScreen(){
+}
+
+//select name
+//select computer
+//select best friend
+//sselect
+
+//thoughts:
+/*
+Classmates/teachers as party
+story based on if zombie apocalypse happened here.
+
+2 versions: zombie apocalypse and coding challenges.
+
+
+
+*/
