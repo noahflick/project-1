d7dc9b22d51a749ea3d7a1ee93b2f7d76c6930a3
Noah Flick <noahflick@gmail.com>
2016-03-22 00:23:41 -0700

    img change when curLocation changes

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 console.log("linked")
 
+
 function randNum(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 var hero = {
-  name: 'Josh',
+  name: prompt("What is your name?"),
   hp: 100,
   actions: ['Run', 'Fight'],
   mood: 'in shock',
@@ -34,25 +35,25 @@ var friends = [
 //machete, axe, chainsaw, nunchuk, crowbar,
 ]
 
-var curLocation = {} //current location. locations should have a forward and backward linked location.
+console.log(friends.length)
 
+for (var i = 0; i < friends.length; i++) {
+  if (friends[i].name.toLowerCase() === hero.name.toLowerCase()){
+    console.log("splicing" + friends[i].name); friends.splice(i, 1);
+  }
+}
 
-var zoms = friends.splice(randNum(0, friends.length-1), 1) // any friends that have been bitten
-
-var status
-var inventory
-var party
-var objective
-var equipped
-
-var screen
-var journalScreen
-var log
-
-var locations = [//Hannah: make a location index object with keys
-{id: 'class3'},
-{id: 'lounge'},
-{id: 'class2'},
+function Location(name, events, loot, nextLoc, prevLoc, img){
+  this.name = name
+  this.events = events
+  this.loot = loot
+  this.nextLoc = nextLoc
+  this.prevLoc = prevLoc
+  this.img = img
+}//Hannah: make a location index object with keys
+var class3 = new Location('Classroom 3', ['panic', 'freakout', 'faint'], 'power strip', 'lounge', 'na', 'http://s3-media4.fl.yelpcdn.com/bphoto/3_KhycVbw8DbUJUOjXmwpA/o.jpg')
+var lounge = new Location('GA Lounge', ['UX Mob', 'savior', 'leader'], 'coffee maker', 'hallway', 'Classroom 3', 'http://s3-media4.fl.yelpcdn.com/bphoto/_plEFiI6ph2EEdI3Z02ypA/o.jpg')
+/*{id: 'class2'},  possible locations
 {id: 'class1'},
 {id: 'hall'},
 {id: 'elevator'},
@@ -72,8 +73,25 @@ var locations = [//Hannah: make a location index object with keys
 {id: 'unionParking'},
 {id: 'neightborSt'},
 {id: 'yourSt'},
-{id: 'yourHome'},
-]
+{id: 'yourHome'},*/
+
+var curLocation = lounge //current location. locations should have a forward and backward linked location.
+
+
+var zoms = friends.splice(randNum(0, friends.length-1), 1) // any friends that have been bitten, starts with zoms[0]
+
+var status
+var inventory
+var party
+var objective
+var equipped
+
+var screen
+var journalScreen
+var log
+
+
+
 
 
 var events = [
@@ -81,6 +99,9 @@ var events = [
 {id: 'friendBitten'},
 {id: 'meet'}
 ]
+
+var inCombat = false
+var inConvo = false
 
 function setup(){
   //ask name
@@ -95,6 +116,7 @@ function initScreen(){
   $('#hp').html('HP: ' + hero.hp)
   $('#effects').html('Effects: ' + hero.effects)
   $('#mood').html('Mood: ' + hero.mood)
+  $('#display').css('background', 'url(' + curLocation.img + ')')
 }
 
 initScreen()
@@ -107,20 +129,20 @@ function update(text){
 
 function introText(){
   update("You are working on bugs... you look up as a decaying WDI 8 student awkwardly ambles into the room and bites " + zoms[0].name + " on the neck. ")
-
 }
+
 introText()
 
-function changeLoc(){
+/*function changeLoc(){
   $('#display').css("background-image", "url('http://s3-media4.fl.yelpcdn.com/bphoto/_plEFiI6ph2EEdI3Z02ypA/o.jpg')")
-}
+}*/
 
 var actions = [ 'run to lounge', 'run to hallway', 'help ' + zoms[0].name, 'cry'
 ]
 
 function makeActionButtons(arr){
   for (var i = 0; i < arr.length; i++) {
-    $("<button>" + arr[i] + "</button>").appendTo('#input')
+    $("<div>" + arr[i] + "</div>").appendTo('#input')
   }
 
 }
